1de643bd17dbd924935b58e5648d848c51e041cf
Lea Dohle <leasod00@zedat.fu-berlin.de>
2025-09-07 14:23:04 +0100

    bellman-ford main function, shortest paths backtracking

--- BF_shortest_path.py
+++ BF_shortest_path.py
@@ -28,6 +28,49 @@ def initialize(G,s):
     dists[s] = 0
     return dists, preds
 
+# u: index of vertex u (int)
+# v: index of vertex v (int)   
+# weight: weight of edge (u,v) (float)
+# dists: array of distances (np.array) 
+# preds: array of predecessors (np.array)
+# relaxes edge (u,v) if possible
+def relax(u,v,weight,dists,preds):
+    if dists[v] > dists[u] + weight:
+        dists[v] = dists[u] + weight
+        preds[v] = u
+
+# G: graph as a dict of dicts
+# s: index of start vertex (int)
+# returns: distances, predecessors
+# or None, None if negative cycle detected
+def belfor(G,s):
+    dists, preds = initialize(G,s)
+    for u in G.keys():
+        for v in G[v].keys():
+            relax(u,v,G[u][v],dists,preds)
+
+    # check for negative cycles
+    for u in G.keys():
+        for v in G[v].keys():
+            if dists[v] > dists[u] + G[u][v]:
+                return None, None
+            
+    return dists, preds
+
+def shortest_path(preds,start,end):
+    current = end
+    path = [end]
+    # iteration counter to prevent infinte loop
+    i = 0
+    while current != start:
+        current = preds[current]
+        path = path + [current]
+        i += 1
+        if i > len(preds):
+           raise Exception("Could not reach starting vertex from destination vertex. Check predecessors.")            
+    
+    return path[::-1]
+
 G = {
     0: {2: 2, 1: 2},   # 0→2 mit Kosten 2, 0→1 mit Kosten 2
     1: {2: -5},        # 1→2 mit Kosten -5
