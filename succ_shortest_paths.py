fd629989442e7cdb4304386537f03ee1ac8a5c81
Lea Dohle <leasophie.dohle@gmail.com>
2025-10-17 12:56:52 +0200

    succ_shortest_paths() is working, using copy.deepcopy() to create graph copies, deleting edges with remaining capacity 0 in residual graph, set flow value of new residual edge to -flow

--- succ_shortest_paths.py
+++ succ_shortest_paths.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 from BF_shortest_path import belfor, shortest_path
 
 # G: graph as a dict of dicts of dicts with 'capacity', 'cost', 'flow' as keys
@@ -8,7 +9,7 @@ from BF_shortest_path import belfor, shortest_path
 # returns: residual graph R after sending flow along path
 def residual_graph(G, path, flow):
     # create a copy of G to work on
-    R = G.copy() 
+    R = copy.deepcopy(G) 
 
     for i in range(len(path)-1): # walk along the path
         u = path[i]
@@ -17,20 +18,18 @@ def residual_graph(G, path, flow):
         # augment flow along edge (u,v)
         R[u][v]['capacity'] -= flow
         R[u][v]['flow'] += flow
+        if R[u][v]['capacity'] == 0:
+            del R[u][v] # remove edge if capacity is 0
 
         # handle residual edge (v,u)
         if u not in R[v]: # add if not present
-            R[v][u] = {'capacity': flow, 'cost': -G[u][v]['cost'], 'flow': 0} 
+            R[v][u] = {'capacity': flow, 'cost': -G[u][v]['cost'], 'flow': -flow} 
         else:
             R[v][u]['capacity'] += flow
-            R[v][u]['cost'] =- G[u][v]['cost']
+            R[v][u]['cost'] = -G[u][v]['cost']
+            R[v][u]['flow'] -= flow
    
     return R
-
-
-
-
-
 
 # G: graph as a dict of dicts of dicts with 'capacity', 'cost', 'flow' as keys
 # i.e. G[u][v]['capacity']: capacity of edge (u,v) (int)
@@ -40,27 +39,23 @@ def residual_graph(G, path, flow):
 # returns: total cost of flow, edges with positive flow as dict with (u,v) as keys and 
 # a dict containing 'flow' and 'cost' of edge
 def succ_shortest_paths(G, source, sink, target_flow):
-    
 
     # create a copy of G to work on
-    R = G.copy()
+    R = copy.deepcopy(G)
     # initialize all flows to 0
     for u in R.keys():
         for v in R[u].keys():
             R[u][v]['flow'] = 0
     
-    
     flow_left = target_flow
 
-    while flow_left >= 0:
-
+    while flow_left > 0:
 
         # use Bellman-Ford to find shortest path from source to sink 
         _, predecessors = belfor(R, source)
         path = shortest_path(predecessors, source, sink)
         if path is None:
             break # No more augmenting paths
-
 
         # calculate minimal capaity of edges on path
         min_capacity = np.inf
@@ -69,7 +64,6 @@ def succ_shortest_paths(G, source, sink, target_flow):
             v = path[i+1]
             if min_capacity > R[u][v]['capacity']:
                 min_capacity = R[u][v]['capacity']
-
         
         # need this minimum, because if flow_left < min_capacity, we send more flow than allowed
         delta = min(min_capacity, flow_left)
@@ -87,8 +81,8 @@ def succ_shortest_paths(G, source, sink, target_flow):
     edges_with_flow = {}
     for u in G.keys():
         for v in G[u].keys():
-            # copy flow from R to G
-            G[u][v]['flow'] = R[u][v]['flow']
+            # take flow residual edge in R to G
+            G[u][v]['flow'] = np.absolute(R[v][u]['flow'])
             # add edge costs to total costs
             cost += G[u][v]['flow']*G[u][v]['cost']
             # save edges with positive flow
@@ -99,16 +93,17 @@ def succ_shortest_paths(G, source, sink, target_flow):
 
 if __name__ == "__main__":
     G = {
-        0: {1: {'flow': 0, 'capacity': 4, 'cost': 3},   # 0→2 mit Kosten 2, Flow & Kapazität 0
-            3: {'flow': 0, 'capacity': 1, 'cost': 2}},  # 0→1 mit Kosten 2, Flow & Kapazität 0
+        0: {1: {'flow': 0, 'capacity': 4, 'cost': 3},
+            3: {'flow': 0, 'capacity': 1, 'cost': 2}},
         1: {2: {'flow': 0, 'capacity': 2, 'cost': 1},
-            3: {'flow': 0, 'capacity': 3, 'cost': 1}}, # 1→2 mit Kosten -1, Flow & Kapazität 0
+            3: {'flow': 0, 'capacity': 3, 'cost': 1}},
 
         3: {2: {'flow': 0, 'capacity': 3, 'cost': 1}},
-        2: {}}   # 3→1 mit Kosten 5, Flow & Kapazität 0
+        2: {}}
             
     cost, edges_with_flow = succ_shortest_paths(G, 0, 2, 4)
  
-
+    print("Total cost:", cost)
+    print(G)
             
         
